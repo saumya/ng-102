b60e596a39a8e8d409f205511e76ca8926210f20
Saumya Ray <saumya@Saumya-MacBook-Pro.local>
2015-10-14 06:04:52 +0530

    WIP

---
+++ js/app/modules/craftsApp.js
@@ -0,0 +1,5 @@
+//	Application Entry Module
+var mApp = angular.module('myCraftsApp',[]);
+mApp.controller('AppController',function($scope){
+	$scope.appinfo = [{name:'craft',version:'1.0.0',author:'saumya'},{name:'craft',version:'1.0.0',author:'saumya'}];
+});
