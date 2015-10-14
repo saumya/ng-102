ff845f03911c991d899d328a37c4d4a4010a23f9
Saumya Ray <saumya@Saumya-MacBook-Pro.local>
2015-10-14 08:09:39 +0530

    INIT: setup is done

--- js/app/modules/craftsApp.js
+++ js/app/modules/craftsApp.js
@@ -1,5 +1,18 @@
-//	Application Entry Module
-var mApp = angular.module('myCraftsApp',[]);
-mApp.controller('AppController',function($scope){
-	$scope.appinfo = [{name:'craft',version:'1.0.0',author:'saumya'},{name:'craft',version:'1.0.0',author:'saumya'}];
-});
+//
+(function(){
+	//	Application Entry Module
+	var mApp = angular.module('myCraftsApp',["ngRoute"]);
+	//	Configuration for the application
+	mApp.config(function($routeProvider,$locationProvider){
+		$locationProvider.hashPrefix('!');
+		$routeProvider.
+			when("/",{templateUrl: "partials/app_home.html"}).
+			when("/login",{templateUrl:"partials/app_login.html",controller:"LoginController"}).
+			otherwise({redirectTo:"/"});
+	});
+	//	Controller
+	mApp.controller('LoginController',function($scope){
+		$scope.appinfo =	[	{name:'craft',version:'1.0.0',author:'saumya'},
+								{name:'craft',version:'1.0.0',author:'saumya'}	];
+	});	
+})();
