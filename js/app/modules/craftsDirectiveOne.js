1f926a5646938421479438e209f4092a60f6986e
saumya <saumya@vmail.e>
2015-11-24 21:33:17 +0530

    Done: Study of the Directives

--- js/app/modules/craftsDirectiveOne.js
+++ js/app/modules/craftsDirectiveOne.js
@@ -4,7 +4,7 @@
   //directiveModule.directive('craftsOne',function(){});
   directiveModule.directive('google',function(){
     var directiveObj = {
-      restrict:'E',
+      restrict:'EAC',
       replace: true,
       template:'<a href="http://google.com">Google</a>'
     };
@@ -15,6 +15,23 @@
       restrict:'E',
       replace: true, // replaces the custom-directive-tag from generated HTML
       templateUrl:"partials/app_directive_one.html"
+    };
+    return directiveObj;
+  });
+  directiveModule.directive('craftsTwoDirective',function(){
+    var directiveObj = {
+      restrict:'EA',
+      replace:true,
+      template:'<a href="{{linkUrl}}">{{linkName}}</a>',
+      scope:{
+        linkUrl:'@',
+        linkName:'@nameToDisplay'
+      },
+      controller:function($scope){
+        console.log('craftsTwoDirective:controller:$scope',$scope);
+        console.log('$scope.linkUrl : ',$scope.linkUrl);
+        console.log('$scope.linkName : ',$scope.linkName);
+      }
     };
     return directiveObj;
   });
